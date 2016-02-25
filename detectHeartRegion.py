f595c4a05648918e0b5e3ac93b3277a9aebb6c82
sunilvengalil <vengalilsunilkumar@gmail.com>
2016-02-25 11:57:38 +0530

    Initial code to detect heart region

---
+++ detectHeartRegion.py
@@ -0,0 +1,17 @@
+import pdb
+from matplotlib import pyplot
+
+#Function to detect the heart region in all the given images
+def detect_heart_region(images):
+    (num_slices, num_times, width,height) = images.shape
+
+    #pyplot.imshow(images[0,0,:,:]);
+    #pyplot.show();
+    
+
+    
+
+    
+    pdb.set_trace()
+
+    return 1
